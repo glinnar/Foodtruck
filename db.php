959f4d484a556ab8935c8fab70301829f7f9f305
Gustaf Linnarsson <g.linnar@gmail.com>
2017-09-28 12:51:38 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 $servername="localhost";
-$dbname="truck";
-$username="root";
+$dbname="";
+$username="";
 $pdo = new PDO("mysql:host=$servername;dbname=$dbname",$username);
 $pdo->setAttribute( PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
